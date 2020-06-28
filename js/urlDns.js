828cf07b6d99cd9191d425d6b5e47cff8fe7355d
yrvh <573118642@qq.com>
2020-06-28 18:10:22 +0800

    vugr

--- js/urlDns.js
+++ js/urlDns.js
@@ -51,3 +51,18 @@ function goToLogin(address) {
 // $.ajaxSetup({
 //     crossDomain: true
 // });
+
+//=============格式化金钱===================
+function formatMoney(number, places, symbol, thousand, decimal) {
+				number = number || 0;
+				places = !isNaN(places = Math.abs(places)) ? places : 2;
+				symbol = symbol !== undefined ? symbol : "$";
+				thousand = thousand || ",";
+				decimal = decimal || ".";
+				var negative = number < 0 ? "-" : "",
+					i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
+					j = (j = i.length) > 3 ? j % 3 : 0;
+				return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
+			}
+
+// console.log(formatMoney(54321)) // $54,321
